f8866c5c5fafc45ed865009580e0b239dd178e45
Sophia White <swhite77@terpmail.umd.edu>
2024-11-08 14:29:27 -0500

    added computer_move function

--- functions.py
+++ functions.py
@@ -24,7 +24,29 @@ def calculate_score(board):
 
 
 def computer_move(ai_board, opponent_board, die_value):
-    #makes the best move for the computer player
+    """
+    Makes the best move for the computer player.
+
+    Args:
+        ai_board: The computer player's board.
+        opponent_board: The opponent player's board.
+        die_value: The value of the die rolled.
+
+    Returns:
+        The column index where the die should be placed.
+    """
+
+    for col_idx, col_values in ai_board.items():
+        
+        if die_value in col_values:
+            return col_idx
+
+        if die_value in opponent_board[col_idx]:
+            return col_idx
+
+    min_score_col = min(ai_board, key=lambda x: sum(ai_board[x]))
+    return min_score_col
+
     
 def cancel_die(board, other_board):
     #removes die from board if opponent places same value die in the corresponding column
