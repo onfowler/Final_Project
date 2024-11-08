5081d2ee8f1c8d163676ca8544bb8864a152cd40
Oscar Fowler <ofowler1@terpmail.umd.edu>
2024-11-08 10:56:29 -0500

    added example board

--- functions.py
+++ functions.py
@@ -6,13 +6,23 @@ class MockFunc:
         if not self.outputs:
             raise RuntimeError(f"No more outputs to return!")
         return self.outputs.pop(0)
-
-
 # example: roll_die = MockFunc([3, 2, 1, 3, 6, 5])
+#*args- takes any positional arguments
+#**kwargs - takesk any keyword arguments
+
+example_board = ({1 : [6,6],
+                  2 : [3, 4, 4], 
+                  3 : [2, 2, 1]})
+
 
 def calculate_score(board):
     #calculates the score of the board
+    #takes board
+    #returns int
     
+
+
+
 def computer_move(ai_board, opponent_board, die_value):
     #makes the best move for the computer player
     
