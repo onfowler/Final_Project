08be2446e55503b99918c013edaad1b3fc474dec
Oscar Fowler <ofowler1@terpmail.umd.edu>
2024-11-07 20:52:55 -0500

    added the file where the 4 functions will be, added the names of the 4 functions

---
+++ functions.py
@@ -0,0 +1,24 @@
+class MockFunc:
+    def __init__(self, outputs):
+        self.outputs = outputs[:]
+        
+    def __call__(self, *args, **kwargs):
+        if not self.outputs:
+            raise RuntimeError(f"No more outputs to return!")
+        return self.outputs.pop(0)
+
+
+# example: roll_die = MockFunc([3, 2, 1, 3, 6, 5])
+
+def calculate_score(board):
+    #calculates the score of the board
+    
+def computer_move(ai_board, opponent_board, die_value):
+    #makes the best move for the computer player
+    
+def cancel_die(board, other_board):
+    #removes die from board if opponent places same value die in the corresponding column
+    
+
+def manage_game(player1_board, player2_board):
+    #plays the game and simulates two players playing
